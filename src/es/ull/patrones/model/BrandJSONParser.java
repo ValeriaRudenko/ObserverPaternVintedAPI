96d61f8041209f47bc91a88dfade97cbcd294dbb
rrrguez <alu0101394763@ull.edu.es>
2023-12-27 10:57:48 +0000

    Checked that the JSON can be parsed properly

--- src/es/ull/patrones/model/BrandJSONParser.java
+++ src/es/ull/patrones/model/BrandJSONParser.java
@@ -9,31 +9,28 @@ import java.util.List;
 public class BrandJSONParser {
     private List<Brand> brandList;
 
-    public BrandJSONParser(String data, int noOfFavouritesValue) {
-        doParsing(data, noOfFavouritesValue);
+    public BrandJSONParser(String data) {
+        doParsing(data);
     }
 
-    public void doParsing(String data, int noOfFavouritesValue) {
+    public void doParsing(String data) {
         JSONArray brandsArray = new JSONArray(data);
         brandList = new ArrayList<>();
         // Assuming each item in the array represents a brand
         for (int i = 0; i < brandsArray.length(); i++) {
             JSONObject brandJson = brandsArray.getJSONObject(i);
-            System.out.println(brandJson);
-            int favourites = brandJson.getInt("favourites");
-
             // Extract product details from the JSON and create corresponding objects
-            if (favourites >= noOfFavouritesValue) {
-                String title = brandJson.getString("title");
-                String slug = brandJson.getString("slug");
-                int favouriteCount = brandJson.getInt("favourite_count");
-                int itemCount = brandJson.getInt("item_count");
-                boolean isVisibleInListings = brandJson.getBoolean("is_visible_in_listings");
-                boolean requiresAutenticityCheck = brandJson.getBoolean("requires_authenticity_check");
-                boolean isLuxury = brandJson.getBoolean("is_luxury");
-                Brand brand = new Brand(title, slug, favouriteCount, itemCount, isVisibleInListings, requiresAutenticityCheck, isLuxury);
-                brandList.add(brand);
-            }
+            // TODO: add condition to do the parsing (min favourites and stuff)
+            String title = brandJson.getString("title");
+            String slug = brandJson.getString("slug");
+            int favouriteCount = brandJson.getInt("favourite_count");
+            int itemCount = brandJson.getInt("item_count");
+            boolean isVisibleInListings = brandJson.getBoolean("is_visible_in_listings");
+            boolean requiresAutenticityCheck = brandJson.getBoolean("requires_authenticity_check");
+            boolean isLuxury = brandJson.getBoolean("is_luxury");
+            Brand brand = new Brand(title, slug, favouriteCount, itemCount, isVisibleInListings, requiresAutenticityCheck, isLuxury);
+            brandList.add(brand);
+            // System.out.println(brand.getTitle());
         }
     }
 
