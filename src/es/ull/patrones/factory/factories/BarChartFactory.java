0685ed45de7e5fb1ce4daaa306d8e36072265deb
ValeriiaRudenko <99447413+ValeriaRudenko@users.noreply.github.com>
2023-12-29 19:23:17 +0000

    Factories

--- src/es/ull/patrones/factory/factories/BarChartFactory.java
+++ src/es/ull/patrones/factory/factories/BarChartFactory.java
@@ -1,4 +1,22 @@
 package es.ull.patrones.factory.factories;
 
-public class BarChartFactory extends ChartFactory {
+import es.ull.patrones.factory.products.barchart.BarChart;
+import es.ull.patrones.factory.products.barchart.FavouritesBarChart;
+import es.ull.patrones.factory.products.barchart.ItemsBarChart;
+import es.ull.patrones.model.Brand;
+
+import javax.swing.*;
+import java.util.List;
+
+public class BarChartFactory implements ChartFactory{
+
+    @Override
+    public JPanel createChart(List<Brand> brands, String type) {
+        if (type.equals("favourites")) {
+            return new FavouritesBarChart(brands);
+        }else if (type.equals("items")) {
+            return new ItemsBarChart(brands);
+        }
+        return null;
+    }
 }
