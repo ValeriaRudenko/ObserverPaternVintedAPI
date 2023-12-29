252b24d587e1a600d6e3228d18660540bbd6bdc1
ValeriiaRudenko <99447413+ValeriaRudenko@users.noreply.github.com>
2023-12-29 18:50:22 +0000

    Update Scoreboard.java
    scoreboard panel

--- src/es/ull/patrones/gui/Scoreboard.java
+++ src/es/ull/patrones/gui/Scoreboard.java
@@ -1,7 +1,24 @@
 package es.ull.patrones.gui;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Scoreboard extends JFrame {
+    private JPanel chartsPanel;
 
+    public Scoreboard() {
+        // Configure the main frame for the scoreboard
+        this.setLayout(new BorderLayout());
+        this.setTitle("Vinted Scoreboard");
+
+        // Panel to display charts
+        chartsPanel = new JPanel();
+        chartsPanel.setLayout(new GridLayout(2, 3)); // You can adjust the layout as needed
+
+        this.add(chartsPanel, BorderLayout.CENTER);
+    }
+
+    public void addChart(JComponent chart) {
+        chartsPanel.add(chart);
+    }
 }
