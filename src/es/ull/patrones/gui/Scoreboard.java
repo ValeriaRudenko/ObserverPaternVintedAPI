e950a168bb976df67a227c4876e0fca1b5f2c44d
Francisco Javier Garcia Gonzalez <alu0101030562@ull.edu.es>
2024-01-06 13:05:28 +0000

    Actualizacion fichero Scoreboard para patron observador

--- src/es/ull/patrones/gui/Scoreboard.java
+++ src/es/ull/patrones/gui/Scoreboard.java
@@ -1,11 +1,16 @@
 package es.ull.patrones.gui;
 
+import es.ull.patrones.observer.BrandObserver;
+
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Scoreboard extends JFrame {
     private JPanel chartsPanel;
-
+    private List<BrandObserver> observers = new ArrayList<>();
+    private String userOptions;
     public Scoreboard() {
         // Configure the main frame for the scoreboard
         this.setLayout(new BorderLayout());
@@ -16,6 +21,22 @@ public class Scoreboard extends JFrame {
         chartsPanel.setLayout(new GridLayout(2, 3)); // You can adjust the layout as needed
 
         this.add(chartsPanel, BorderLayout.CENTER);
+    }
+
+    public void setUserOptions(String options) {
+        this.userOptions = options;
+        notifyObservers();
+    }
+
+    // Métodos para observadores
+    public void registerObserver(BrandObserver observer) {
+        observers.add(observer);
+    }
+
+    private void notifyObservers() {
+        for (BrandObserver observer : observers) {
+            observer.updateOptions(userOptions);
+        }
     }
 
     public void addChart(JComponent chart) {
